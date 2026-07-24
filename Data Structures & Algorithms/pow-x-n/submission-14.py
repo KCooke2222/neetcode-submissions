5eb846a56685c01608854f5d9c647e093fc05af2
Kevin Cooke <136854995+KCooke2222@users.noreply.github.com>
2026-07-24 12:24:53 -0700

    Add: pow-x-n - submission-14

---
+++ Data Structures & Algorithms/pow-x-n/submission-14.py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        negative = n < 0
+        n = abs(n)
+
+        if x == 0: return 0
+
+        def dfs(x, n):
+            if n == 0:
+                return 1
+            if n == 1:
+                return x
+
+            remainder = x if n % 2 else 1
+            split = dfs(x, n // 2)
+            return split * split * remainder
+
+        res = dfs(x, n)
+        return 1/res if negative else res
