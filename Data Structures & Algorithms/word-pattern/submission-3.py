641c398f04f34e260267ed3f2493ec591fd72d04
Kevin Cooke <136854995+KCooke2222@users.noreply.github.com>
2026-08-13 17:26:21 -0700

    Add: word-pattern - submission-3

---
+++ Data Structures & Algorithms/word-pattern/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        # dictionary word to letter
+        # letters set built during iteration
+        # split s into words
+        # iterate over words
+            # determine results here if word already found and matches letter else...
+
+        
+        wordMap = {}
+        letters = set()
+
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+
+
+        for i in range(len(words)):
+            l = pattern[i]
+            w = words[i]
+
+            if w in wordMap:
+                if l != wordMap[w]:
+                    return False
+            else:
+                if l in letters:
+                    return False
+                else:
+                    wordMap[w] = l
+                    letters.add(l)
+
+        return True
+                
