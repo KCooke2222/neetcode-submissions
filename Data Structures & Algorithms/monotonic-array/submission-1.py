6cc8b05c538022c351331bb6d891f51c288c1794
Kevin Cooke <136854995+KCooke2222@users.noreply.github.com>
2026-08-24 07:27:40 -0700

    Add: monotonic-array - submission-1

---
+++ Data Structures & Algorithms/monotonic-array/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        dir = None
+        for i in range(1, len(nums)):
+            curDir = nums[i] - nums[i - 1]
+            if curDir == 0:
+                    continue
+            
+            if dir == None:
+                dir = curDir
+            else:
+                if (dir > 0) != ( curDir > 0):
+                    return False
+
+        return True
